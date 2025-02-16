995d08fdf9d912fe5fa163c33cea1b6db708460a
Reinald Van Praet <reinald.vanpraet@gmail.com>
2025-02-16 17:31:01 -0600

    Finished the tutorial

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+}
